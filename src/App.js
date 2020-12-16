133af0bd852c3f399d4e3dfc97bcbc278f7a8477
Kazushi Isogai <kazu150@gmail.com>
2020-12-16 12:17:55 +0900

    CounterHook.js追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Counter from './components/Counter'
+import CounterHook from './components/CounterHook'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <CounterHook />
     </div>
   );
 } 
